196a6d38b90241ff580ffd8d334ea913e980dbe2
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-01-26 14:38:52 +0200

    1:07

--- CV19WpfApp/ViewModels/MainWindowViewModel.cs
+++ CV19WpfApp/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using CV19WpfApp.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CV19WpfApp.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title;
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get { return _Title; }
+            //set 
+            //{
+            //    //if(Equals(_Title,value)) return;
+            //    //_Title = value;
+            //    // OnPropertyChanged();
+
+            //    Set(ref _Title, value);
+            //}
+            set => Set(ref _Title,value);
+        }
+        #endregion
+
+
+
 
     }
 }
