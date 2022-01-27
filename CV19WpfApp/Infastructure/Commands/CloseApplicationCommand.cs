8257a2dbf657b56eb5c879cacca1ac2d460c3b9a
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-01-27 11:49:08 +0200

    Определенн класс отдельной команды

---
+++ CV19WpfApp/Infastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,12 @@
+﻿using CV19WpfApp.Infastructure.Commands.Base;
+using System.Windows;
+
+namespace CV19WpfApp.Infastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+        public override void Execute(object parameter)=> Application.Current.Shutdown();
+    }
+}
