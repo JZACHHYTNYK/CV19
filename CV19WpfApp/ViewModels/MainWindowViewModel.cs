61986c71e3252e7aa753cab1ab81a06022139ea9
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-01-27 10:29:31 +0200

    Базовый класс команды

--- CV19WpfApp/ViewModels/MainWindowViewModel.cs
+++ CV19WpfApp/ViewModels/MainWindowViewModel.cs
@@ -8,7 +8,7 @@ namespace CV19WpfApp.ViewModels
     internal class MainWindowViewModel : ViewModel
     {
         #region Заголовок окна
-        private string _Title="Анализ статистики СV19";
+        private string _Title = "Анализ статистики СV19";
         /// <summary>Заголовок окна</summary>
         public string Title
         {
@@ -21,9 +21,24 @@ namespace CV19WpfApp.ViewModels
 
             //    Set(ref _Title, value);
             //}
-            set => Set(ref _Title,value);
+            set => Set(ref _Title, value);
         }
         #endregion
+
+        #region Status : string - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get { return _Status; }
+            set => Set(ref _Status, value);
+        }
+        #endregion
+
+
+
 
 
 
