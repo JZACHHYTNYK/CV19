1d6c93a4794e0c5294e8142d91be27fc1b03a46f
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-01-27 12:07:14 +0200

    2:26

--- CV19WpfApp/Infastructure/Commands/LambdaCommand.cs
+++ CV19WpfApp/Infastructure/Commands/LambdaCommand.cs
@@ -14,6 +14,7 @@ namespace CV19WpfApp.Infastructure.Commands
             _CanExecute = CanExecute;
         }
         
+
         public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true ;
 
         public override void Execute(object parameter)=>_Execute(parameter);
