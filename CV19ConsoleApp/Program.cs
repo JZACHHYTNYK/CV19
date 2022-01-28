26b380519fec11b38f3c808170f56916be341593
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-01-28 14:34:50 +0200

    Запрос к удаленному серверу для получения сырых данных

--- CV19ConsoleApp/Program.cs
+++ CV19ConsoleApp/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace CV19ConsoleApp
 {
     internal class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worl!");
+            //WebClient webClient = new WebClient();
+           
+            var client = new HttpClient();
+
+            var response=client.GetAsync(data_url).Result;
+            var csv_str=response.Content.ReadAsStringAsync().Result;
+
+
+            Console.WriteLine(csv_str);
         }
     }
 }
