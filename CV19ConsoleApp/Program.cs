dda727dab267b0d50a38e80c6a6d881cb35e047e
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-01-28 15:18:26 +0200

    3:31

--- CV19ConsoleApp/Program.cs
+++ CV19ConsoleApp/Program.cs
@@ -1,23 +1,63 @@
 ﻿using System;
-using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CV19ConsoleApp
 {
     internal class Program
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+
+        // считуем поток текстовых данных / формирует поток дынных
+        private static async Task<Stream> GetDataStream()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(data_url, HttpCompletionOption.ResponseHeadersRead);
+            return await response.Content.ReadAsStreamAsync();
+        }
+
+        //разбиение на строки 
+        private static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+            while (!data_reader.EndOfStream)
+            {
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
+        }
+
+        // пулучаем все даты по которым будут разбиты данные
+        private static DateTime[] GetDatas() => GetDataLines()
+            .First()    // берем первую строку
+            .Split(',') // разделитель по запятой получаем массив строк каждый с которых содержит зоголовок каждой колоныки данных csv файла
+            .Skip(4)    //отбросить первые 4 потому что там находятся названия провинции, название страны,  широта и долгота этой странны
+            .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
+            .ToArray();
+
+
         static void Main(string[] args)
         {
             //WebClient webClient = new WebClient();
-           
-            var client = new HttpClient();
 
-            var response=client.GetAsync(data_url).Result;
-            var csv_str=response.Content.ReadAsStringAsync().Result;
+            //foreach (var data_line in GetDataLines())
+            //{
+            //    Console.WriteLine(data_line);
+            //}
+
+            var dates=GetDatas();// полученно только первую строку с csv файла остальные даже скачиваться не начали
+            Console.WriteLine(string.Join("\r\n",dates));
 
 
-            Console.WriteLine(csv_str);
+            Console.ReadLine();
         }
     }
 }
