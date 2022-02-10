96cff914394899bb9d4ead3d494a2cd6d8418583
OLEKSII MIR <jzachhytnyk@outlook.com>
2022-02-10 13:07:47 +0200

    Ошибка

--- TestWpfApp/MainWindow.xaml.cs
+++ TestWpfApp/MainWindow.xaml.cs
@@ -3,6 +3,6 @@
 
     public partial class MainWindow
     {
-        public MainWindow() => InitializeComponent();bfdhgbdfhgdf
+        public MainWindow() => InitializeComponent();bfdhgbdfhgdfфыва
     }
 }
